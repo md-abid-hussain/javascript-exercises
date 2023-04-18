f7f2ce8b030c59eb7e32bcc4628c2368106302aa
Md Abid Hussain <abid0hussain1733@gmail.com>
2023-04-18 23:45:29 +0530

    completed the function to reverse a String

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function (str) {
+    const strArray = str.split('');
+    for (let i = 0; i < strArray.length / 2; i++) {
+        let temp = strArray[i];
+        strArray[i] = strArray[strArray.length - i - 1];
+        strArray[strArray.length - i - 1] = temp;
+    }
+    return strArray.toString().replaceAll(',', '');
 };
 
 // Do not edit below this line

c432d67ac95fa845df2d613b5621b0e9dc967f44
Md Abid Hussain <abid0hussain1733@gmail.com>
2023-04-23 22:55:30 +0530

    completed logic to removeFromArray function

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (array) {
+    const args = Object.values(arguments);
+    for (let i = 1; i < args.length; i++) {
+        let temp = array.indexOf(args[i]);
+        if (temp !== -1) {
+            array.splice(temp, 1);
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line

cb692c2a2b00d8d5b8969ae9544d3a310cc7f15d
Md Abid Hussain <abid0hussain1733@gmail.com>
2023-04-23 23:17:26 +0530

    completed logic for sumAll function

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,8 @@
-const sumAll = function() {
-
+const sumAll = function (arg1, arg2) {
+    if (typeof (arg1) !== 'number' || typeof (arg2) !== 'number' || arg1 < 0 || arg2 < 0)
+        return 'ERROR';
+    let num = Math.abs(arg1 - arg2) + 1;
+    return (num * (arg1 + arg2)) / 2;
 };
 
 // Do not edit below this line

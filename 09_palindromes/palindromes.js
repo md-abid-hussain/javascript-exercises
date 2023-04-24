bf341930de75668e240cfa12107199b941f6bce5
Abid <abid0hussain1733@gmail.com>
2023-04-24 22:31:17 +0530

    completed challenge to check for palindrome

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    str = str.replaceAll(',','').replaceAll(' ','').replaceAll('!','').replaceAll('.','').toLowerCase();
+    for(let i = 0;i<str.length/2;i++){
+        if(str[i]!==str[str.length-i-1])
+            return false;
+    }
+    return true;
 };
 
 // Do not edit below this line

cddae1f25ae045db70d190ab0beebbbebc2ec66c
Abid <abid0hussain1733@gmail.com>
2023-04-24 22:20:49 +0530

    completed challenge for calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,35 @@
-const add = function() {
-	
+const add = function(a,b) {
+  return a+b;	
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	let sum =0;
+  arr.forEach(ele=>{
+    sum = sum+ele;
+  })
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  let prod =1;
+  arr.forEach(ele=>{
+    prod = prod*ele;
+  })
+  return prod;
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return a**b;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if(num <2)	
+    return 1;
+  return num*factorial(num-1);
 };
 
 // Do not edit below this line

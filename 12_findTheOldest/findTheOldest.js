e10f2ee02b643c166c487649bd05e45c7c346048
Abid <abid0hussain1733@gmail.com>
2023-04-30 10:37:21 +0530

    completed

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,18 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (arr) {
+  const initial = arr[0];
+  return arr.reduce((returns, currentVal) => {
+    const current =
+      (returns.yearOfDeath ?? new Date().getFullYear()) - returns.yearOfBirth >
+        (currentVal.yearOfDeath ?? new Date().getFullYear()) -
+          currentVal.yearOfBirth ?? 0
+        ? returns
+        : currentVal;
+    return current;
+  }, initial);
 };
+
+let date = new Date();
+console.log(date.getFullYear());
 
 // Do not edit below this line
 module.exports = findTheOldest;
